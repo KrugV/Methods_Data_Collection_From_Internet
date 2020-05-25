3aee8b0eefd608ab311f4bee68714da951218d8e
KrugV <nvtel@yandex.ru>
2020-05-25 21:01:42 +0300

    Скрипт по ДЗ 4-го урока.

---
+++ les_4.py
@@ -0,0 +1,13 @@
+import requests
+import urllib.request
+
+from lxml.html import fromstring
+
+URL = 'https://yandex.ru/search/'
+HTML = urllib.request.urlopen(URL)
+
+list_html = HTML.read().decode('utf-8')
+parser = fromstring(list_html)
+
+for elem in parser.xpath("//a[contains(@class,'link_cropped_no')]/@href | //a[contains(@class,'organic__url_type_multiline')]/@href"):
+    print(elem.text)
